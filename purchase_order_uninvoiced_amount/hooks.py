13d21a17636d63fd5a1a2db8c02a803b9a009d03
OscarIndias <oscar.indias@factorlibre.com>
2023-02-10 14:20:39 +0100

    [UPDATE] purchase_order_uninvoiced_amount

---
+++ purchase_order_uninvoiced_amount/hooks.py
@@ -0,0 +1,8 @@
+# © 2023 - FactorLibre - Oscar Indias <oscar.indias@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+def pre_init_hook(cr):
+    cr.execute(
+        """ALTER TABLE sale_order_line
+        ADD COLUMN IF NOT EXISTS amount_uninvoiced numeric DEFAULT 0.0"""
+    )
